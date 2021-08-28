5c91bc6bfa95fe224a3c7a3f39f8b59a93e936f4
Mohammed Farish <farishmusthafa@gmail.com>
2021-08-28 17:59:45 +0400

    loader added

--- src/Pages/Landing Page/payment/PaymentPage.jsx
+++ src/Pages/Landing Page/payment/PaymentPage.jsx
@@ -59,7 +59,11 @@ const PaymentPage = ({ match, setSiteTitle, setSiteContent }) => {
   }, []);
 
   if (loading) {
-    return <div />;
+    return (
+      <div className="payment-page-loading">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm8 12c0 4.418-3.582 8-8 8s-8-3.582-8-8 3.582-8 8-8 8 3.582 8 8zm-19 0c0-6.065 4.935-11 11-11v2c-4.962 0-9 4.038-9 9 0 2.481 1.009 4.731 2.639 6.361l-1.414 1.414.015.014c-2-1.994-3.24-4.749-3.24-7.789z" /></svg>
+      </div>
+    );
   }
 
   return (
