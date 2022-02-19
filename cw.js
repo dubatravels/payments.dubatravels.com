b3b00bc0c0c78a35aa354357cb9090748b5fc56e
Mohammed Farish <farishmusthafa@gmail.com>
2022-02-20 01:15:17 +0400

    Updates

---
+++ cw.js
@@ -0,0 +1,18 @@
+/* eslint-disable func-names */
+if (navigator.userAgent !== "ReactSnap") {
+  (function (d, t) {
+    const BASE_URL = "https://chat.dubatravels.com";
+    const g = d.createElement(t); const
+      s = d.getElementsByTagName(t)[0];
+    g.src = `${BASE_URL}/packs/js/sdk.js`;
+    g.defer = true;
+    g.async = true;
+    s.parentNode.insertBefore(g, s);
+    g.onload = function () {
+      window.chatwootSDK.run({
+        websiteToken: "6iPVdTxdpfmoE54ai2GDzkZD",
+        baseUrl: BASE_URL,
+      });
+    };
+  }(document, "script"));
+}
